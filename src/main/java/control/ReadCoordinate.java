0bf4785ad04c7b3dc662770abb4ead5d3db0db48
Nikita <nik_krik@ukr.net>
2015-05-02 22:43:28 +0300

    change read coordinate logic

--- src/main/java/control/ReadCoordinate.java
+++ src/main/java/control/ReadCoordinate.java
@@ -1,14 +1,16 @@
 package control;
 
+import view.ConsoleRead;
+
 public class ReadCoordinate extends ConsoleRead {
 
     private static int CoordinateX;
 
     private static int CoordinateY;
 
-    private static String setX = "please enter X coordinate: ";
+    private static final String SET_X= "please enter X coordinate: ";
 
-    private static String setY = "please enter Y coordinate: ";
+    private static final String SET_Y = "please enter Y coordinate: ";
 
 
     public static int getCoordinateX() {return CoordinateX;    }
@@ -16,10 +18,10 @@ public class ReadCoordinate extends ConsoleRead {
     public static int getCoordinateY() {return CoordinateY;    }
 //todo check for int
     public static void setCoordinateX() {
-        CoordinateX = Integer.parseInt(readFromConsole(setX));
+        CoordinateX = Integer.parseInt(readFromConsole(SET_X));
     }
 
     public static void setCoordinateY() {
-        CoordinateY = Integer.parseInt(readFromConsole(setY));
+        CoordinateY = Integer.parseInt(readFromConsole(SET_Y));
     }
 }
