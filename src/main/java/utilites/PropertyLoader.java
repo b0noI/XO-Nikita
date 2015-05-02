028b2cc21df406c1417d9f8f3efe84eeffc6a690
Nikita <nik_krik@ukr.net>
2015-05-03 00:20:57 +0300

    add log4test add property loader chage to pom.xml

---
+++ src/main/java/utilites/PropertyLoader.java
@@ -0,0 +1,26 @@
+package utilites;
+
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyLoader {
+    private static final String PROPERTY_FILE = "/application.properties";
+
+    public static String loadProperty(String name)
+    {
+        Properties props = new Properties();
+        try {
+            props.load(PropertyLoader.class.getResourceAsStream(PROPERTY_FILE));
+        } catch (IOException e) {
+            Assert.fail(Log4Test.info(name));
+        }
+        String value = "";
+        if (name != null)
+        {
+            value = props.getProperty(name);
+        }
+        return value;
+    }
+}
