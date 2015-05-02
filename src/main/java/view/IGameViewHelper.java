66777f8adaf332f6897f7af60048843353de1f99
Nikita <nik_krik@ukr.net>
2015-05-02 17:50:35 +0300

    add 2 interface for refactor

---
+++ src/main/java/view/IGameViewHelper.java
@@ -0,0 +1,6 @@
+package view;
+
+public interface IGameViewHelper {
+
+    public int getCoordinate();
+}
---
+++ src/main/java/view/IUserViewHelper.java
@@ -0,0 +1,6 @@
+package view;
+
+public interface IUserViewHelper {
+
+    public String getPlayerName();
+}
